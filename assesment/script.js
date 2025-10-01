71012e0f9497192cfeead65f18e0d21f853061ec
Ankit Gupta <ankitjgupta57@gmail.com>
2025-10-01 15:43:19 +0530

    Add files via upload

---
+++ assesment/script.js
@@ -0,0 +1,45 @@
+function breakfast(){
+    setTimeout(()=>{
+             console.log("Breakfast Prepared");
+    },1000);
+   
+}
+
+breakfast();
+
+
+function wash(){
+    
+    setTimeout(() => {
+    console.log("Wash Dishes");
+}, 2000);
+}
+
+
+wash();
+
+
+
+function coffee(){
+    
+    setTimeout(() => {
+    console.log("Coffee Prepared");
+},  3000);
+}
+
+coffee();
+
+
+
+
+
+function start(){
+    
+    setTimeout(() => {
+    console.log("Starting New Task");
+},  8000);
+}
+
+start();
+// let arr = new Array
+// console.log(breakfast(),arr);
